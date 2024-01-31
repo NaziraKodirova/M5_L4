fe25a6fbc794bfde24867c293f9550dc0295494e
Nazira <nazira.kodirova@mail.ru>
2024-01-31 21:54:59 +0500

    added azimjonningqoylari.py and toshlar.py

---
+++ azimjonningqoylari.py
@@ -0,0 +1,7 @@
+n = int(input())
+x = 0
+if n % 4 == 0:
+    x = n // 2
+else:
+    x = -1
+print(x)
---
+++ toshlar.py
@@ -0,0 +1,6 @@
+N = int(input())
+
+if N % 2 == 0 :
+    print("Second player")
+else:
+    print("First player")
